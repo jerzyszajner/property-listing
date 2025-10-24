a68d3918170f7ac8c130dca469b41c928328a625
jerzy szajner <jerzy.szajner@gmail.com>
2025-10-24 19:43:13 +0200

    add property service

---
+++ src/services/propertyService.ts
@@ -0,0 +1,13 @@
+import type { Property } from "../types/property";
+
+export const fetchProperties = async (): Promise<Property[]> => {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/property-listing-data.json"
+  );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
