150dfd34ea05d121e3b97407a70d6e7c7c18efab
jerzy szajner <jerzy.szajner@gmail.com>
2025-11-03 21:06:45 +0100

    improve not fund page styling and layout

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+import notFoundImage from "@/assets/images/not-found.webp";
 import styles from "./NotFound.module.css";
 
 /* Not found page */
 const NotFound = () => {
   return (
     <div className={styles.notFound}>
-      Page Not Found Bro! Check the URL and Try Again!
+      <div className={styles.wrapper}>
+        <img src={notFoundImage} alt="Not Found" className={styles.image} />
+        <Link to="/" className={styles.link}>
+          Go to Home
+        </Link>
+      </div>
     </div>
   );
 };
