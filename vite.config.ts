5d3a776913068662b2a846e6eba0c81ec0081868
jerzy szajner <jerzy.szajner@gmail.com>
2025-11-07 10:12:43 +0100

    improve property details responsive layout

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [".ngrok-free.app"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
