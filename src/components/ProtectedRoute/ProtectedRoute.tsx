32181196ae29d622bf9202c349843193bb7a2faa
jerzy szajner <jerzy.szajner@gmail.com>
2025-12-04 18:38:41 +0100

    add email verification gate to ProtectedRoute component

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuthContext } from "@/contexts/AuthContext";
 import Spinner from "../Spinner/Spinner";
 
@@ -9,15 +9,31 @@ interface ProtectedRouteProps {
 /* ProtectedRoute component */
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const { user, isLoading } = useAuthContext();
+  const location = useLocation();
 
   if (isLoading) {
     return <Spinner />;
   }
 
   if (!user) {
-    return <Navigate to="/sign-in" replace />;
+    return (
+      // Save current location for redirect after sign-in
+      <Navigate to="/sign-in" state={{ from: location.pathname }} replace />
+    );
   }
-  return <>{children}</>;
+
+  if (!user.emailVerified) {
+    return (
+      <Navigate
+        to="/email-verification"
+        // Save current location for redirect after sign-in
+        state={{ from: location.pathname }}
+        replace
+      />
+    );
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
