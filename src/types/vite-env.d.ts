c108e21927e3f3270668bd47608abd7f240dece9
jerzy szajner <jerzy.szajner@gmail.com>
2025-12-12 23:52:15 +0100

    add cloudinary environment variable to types

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -10,6 +10,7 @@ interface ImportMetaEnv {
 
   readonly VITE_GOOGLE_MAPS_API_KEY: string;
   readonly VITE_GEMINI_API_KEY: string;
+  readonly VITE_CLOUDINARY_NAME: string;
 }
 
 interface ImportMeta {
